27b4f40c72d5f11925d7f38d8c90549afee3eccf
Engländer, Leon <leon.englaender@t-online.de>
2023-03-01 10:47:50 +0100

    Print Red Console Warning

--- start_Windows.bat
+++ start_Windows.bat
@@ -3,7 +3,14 @@ python -m venv venv
 call .\venv\Scripts\activate
 pip install -r requirements.txt
 cls
-echo Press CTRL+C to stop the app!
+
+color 0C
+echo ""
+echo "The dashboard will now load in your browser. Please do not close this console window - it must be kept open during the whole analysis."
+echo "Press CTRL+C to stop the app!"
+echo ""
+color 08
+
 python main.py
 deactivate
 cls
